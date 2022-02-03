34dcce939eea669edc1f216f49b2b9a659d0ec02
Bojan Gabric <bojan@bojangabric.com>
2022-02-03 08:44:12 +0100

    fix: don't show successful payment if no transaction data

--- src/components/steps/transaction/index.js
+++ src/components/steps/transaction/index.js
@@ -2,7 +2,8 @@ import { connect } from 'react-redux';
 import PendingPayment from './pending';
 import SuccessfulPayment from './success';
 
-const Transaction = ({ transaction }) => (transaction === 'pending' ? <PendingPayment /> : <SuccessfulPayment />);
+const Transaction = ({ transaction }) =>
+  !transaction || transaction === 'pending' ? <PendingPayment /> : <SuccessfulPayment />;
 
 const mapStateToProps = state => ({ transaction: state.payment.transaction });
 
