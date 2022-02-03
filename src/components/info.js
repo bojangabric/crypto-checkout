a73c97a3fa31a6b7223c2fc4de8a4a8ee2992b61
Bojan Gabric <bojan@bojangabric.com>
2022-02-03 08:28:55 +0100

    feat: info (right) side of app

---
+++ src/components/info.js
@@ -0,0 +1,12 @@
+const Info = () => (
+  <div className="bg-white p-[50px] drop-shadow-info" style={{ flex: '0 0 390px' }}>
+    <p className="text-primary-dark text-[17px] font-medium">Easy, fast and secure payments</p>
+    <p className="mt-6 text-sm leading-relaxed text-medium-gray">
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+    <p className="mt-[34px] text-center text-sm leading-loose text-medium-gray">Powered by</p>
+    <img src="/images/bitpay.png" className="mx-auto mt-1.5 w-[129px]" />
+  </div>
+);
+
+export default Info;
