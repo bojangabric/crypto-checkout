71b79b3afd1e89498c2829b09c57e6164666fa77
Bojan Gabric <bojan@bojangabric.com>
2022-02-03 08:47:40 +0100

    fix: remove transaction from GET_CURRENCIES

--- src/redux/reducers/payment.js
+++ src/redux/reducers/payment.js
@@ -14,7 +14,6 @@ const payment = (
     case t.GET_CURRENCIES:
       return {
         ...state,
-        transaction: 'successful',
         currencies: action.payload
       };
     case t.SET_TOTAL:
